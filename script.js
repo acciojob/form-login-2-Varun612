0bb6b9604ce241530c8c23df97e7fdefa099d4de
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-17 00:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function submit() {
+	const firstName=document.getElementById('firstName').value;
+	const lastName=document.getElementById('lastName').value;
+	const phoneNumber=document.getElementById('phone').value;
+	const email=document.getElementById('email').value;
+	alert(`First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`)
+}
